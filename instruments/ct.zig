jturcotte/chiptrack

commit bd552fe120fc78edbc8ba17b5975c6d74b0eded0

Stop the wave channel instead of switching banks
RBoy now ignores writes while playing so better wait to use
the banks until I implement support for this GBA extension
in my RBoy branch.

instruments/ct.zig
@@ -289,7 +289,6 @@ pub const gba = struct {
         return WavTable{ .v = @bitCast(@byteSwap(t)) };
     }
 
-    var current_bank: u1 = 0;
     /// (NR30) - Channel 3 Stop/Wave RAM select (R/W)
     pub const WaveRam = packed struct {
         ///  Bit        Expl.
@@ -323,11 +322,9 @@ pub const gba = struct {
             gba_set_sound_reg(address(channel), @as(u16, @bitCast(self)));
         }
         pub fn setTable(table: *const WavTable) void {
-            // Write to the unselected bank
+            (WaveRam{ .playing = 0 }).writeTo(wave);
             gba_set_wave_table(&table.v, table.v.len);
-            // Then select it
-            current_bank ^= 1;
-            (WaveRam{ .playing = 1, .bank = current_bank }).writeTo(wave);
+            (WaveRam{ .playing = 1 }).writeTo(wave);
         }
     };
 

commit de7f2e2b750626870652b7a44fc32e5c2999cc8e

gba: Fix the wave table not always being used for playback
This is a regression from bd552fe120fc78edbc8ba17b5975c6d74b0eded0

instruments/ct.zig
@@ -338,9 +338,9 @@ pub const gba = struct {
             gba_set_sound_reg(address(channel), @as(u16, @bitCast(self)));
         }
         pub fn setTable(table: *const WavTable) void {
-            (WaveRam{ .playing = 0 }).writeTo(wave);
+            (WaveRam{ .playing = 0, .bank = 0 }).writeTo(wave);
             gba_set_wave_table(&table.v, table.v.len);
-            (WaveRam{ .playing = 1 }).writeTo(wave);
+            (WaveRam{ .playing = 1, .bank = 1 }).writeTo(wave);
         }
     };
 